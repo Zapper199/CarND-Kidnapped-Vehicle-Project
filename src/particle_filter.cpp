b8a9677e9c9a7638e6b38ea62309a11ec832eb6a
Jared Jensen <j@redj.xyz>
2017-04-29 22:08:46 -0700

    feat: Add update weights step
    The update weights step takes in a new set of observations, finds the nearest landmark, and updates all weights accordingly.

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -9,6 +9,8 @@
 #include <algorithm>
 #include <iostream>
 #include <numeric>
+#include <cfloat>
+
 
 #include "particle_filter.h"
 
@@ -19,6 +21,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
 	num_particles = 100;
 	particles.resize(num_particles);
+	weights.resize(num_particles, 0);
 
 	// Initialize all particles to this position
 	std::default_random_engine gen;
@@ -45,7 +48,7 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 	//  http://www.cplusplus.com/reference/random/default_random_engine/
 
 	std::default_random_engine gen;
-	for (Particle particle : particles) {
+	for (Particle &particle : particles) {
 		double x_f = particle.x + (velocity/yaw_rate) * (sin(particle.theta + yaw_rate * delta_t) - sin(particle.theta));
 		double y_f = particle.y + (velocity/yaw_rate) * (cos(particle.theta) - cos(particle.theta + yaw_rate * delta_t));
 		double theta_f = particle.theta + yaw_rate * delta_t;
@@ -65,6 +68,20 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 	//   observed measurement to this particular landmark.
 	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to
 	//   implement this method and use it as a helper during the updateWeights phase.
+	for (LandmarkObs &observation : observations) {
+		int closestLandmark;
+		double smallestDistance = DBL_MAX;
+		for (LandmarkObs &prediction : predicted) {
+			double distance = dist(observation.x, observation.y, prediction.x, prediction.y);
+			if (distance <= smallestDistance) {
+				smallestDistance = distance;
+				closestLandmark = prediction.id;
+			}
+		}
+
+		observation.id = closestLandmark;
+	}
+
 
 }
 
@@ -81,6 +98,55 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   3.33. Note that you'll need to switch the minus sign in that equation to a plus to account
 	//   for the fact that the map's y-axis actually points downwards.)
 	//   http://planning.cs.uiuc.edu/node99.html
+
+	// For each particle
+	for (int i = 0; i < particles.size(); i++) {
+		double sum = 0;
+		Particle particle = particles[i];
+
+		// Make a list of predicted measurements for this particle
+		std::vector<LandmarkObs> predicted(map_landmarks.landmark_list.size());
+		for (int j = 0; j < predicted.size(); j++) {
+			Map::single_landmark_s landmark = map_landmarks.landmark_list[j];
+
+			double x_diff = landmark.x_f - particle.x;
+			double y_diff = landmark.y_f - particle.y;
+
+			double x_pred = y_diff * sin(particle.theta) + x_diff * cos(particle.theta);
+			double y_pred = y_diff * cos(particle.theta) - x_diff * sin(particle.theta);
+
+			predicted[j].x = x_pred;
+			predicted[j].y = y_pred;
+			predicted[j].id = landmark.id_i;
+		}
+
+		// Find closest landmarks for each observation
+		dataAssociation(predicted, observations);
+
+		// For each observation, find the error
+		for (LandmarkObs obs : observations) {
+			// Find the probability of the observation
+
+			// First transform observation to map coordinates
+			double obs_x = particle.x + obs.x * cos(particle.theta);
+			double obs_y = particle.y = obs.y * sin(particle.theta);
+
+			// What's the probability that I make this observation?
+			double true_x = map_landmarks.landmark_list[obs.id-1].x_f;
+			double true_y = map_landmarks.landmark_list[obs.id-1].y_f;
+
+			double obs_prob = (1/(2*M_PI*std_landmark[0]*std_landmark[1]))
+				* exp(-(((true_x - obs_x)*(true_x-obs_x))/(2*std_landmark[0]*std_landmark[0]))
+					+ (((true_y - obs_y)*(true_y-obs_y))/(2*std_landmark[1]*std_landmark[1])));
+			weights[i] *= obs_prob;
+		}
+	}
+
+	// Normalize weights
+	double sum = 0;
+	for (int i = 0; i < weights.size(); i++) sum += weights[i];
+	for (int i = 0; i < weights.size(); i++) weights[i] /= sum;
+
 }
 
 void ParticleFilter::resample() {
